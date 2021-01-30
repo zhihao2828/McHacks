9b5da5ed44da46532edbf806d2829436ac00b0df
AlySaleh1 <78103711+AlySaleh1@users.noreply.github.com>
2021-01-30 19:08:45 +0200

    Update Hospital.java
    I added these fields:
    private int minutesToArrive; //how far away is the hospital in minutes
    private double distance; //how far away the user is from the hospital

--- Hospital.java
+++ Hospital.java
@@ -7,12 +7,16 @@ public class Hospital {
     private boolean hasSpots;
     private String[] numSpots; //String of length 2, numAvailableSpots[0] = number of beds currently in use, numAvailableSpots[1] = total number of beds in a hospital
     private String[] location; //TODO update so it uses class Location or something that google understands
+    private int minutesToArrive; //how far away is the hospital in minutes
+    private double distance; //how far away the user is from the hospital
 
-    public Hospital(String name, boolean hasSpots, String[] numSpots, String[] location) {
+    public Hospital(String name, boolean hasSpots, String[] numSpots, String[] location, int min, double distance) {
         this.name = name;
         this.hasSpots = hasSpots;
         this.numSpots = numSpots;
         this.location = location;
+        this.minutesToArrive = min;
+        this.distance = distance;
     }
 
     //setters and getters start
@@ -47,5 +51,21 @@ public class Hospital {
     public void setLocation(String[] location) {
         this.location = location;
     }
+    public int getMinutesToArrive() {
+        return minutesToArrive;
+    }
+
+    public void setMinutesToArrive(int minutesToArrive) {
+        this.minutesToArrive = minutesToArrive;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
     //setters and getters end
 }
