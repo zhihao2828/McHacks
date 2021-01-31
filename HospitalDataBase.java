3b404c8b0ae444854ff4b647d94fa178786831b9
Zhihao Huang <66397820+zhihao2828@users.noreply.github.com>
2021-01-31 00:02:39 -0500

    Add files via upload

--- HospitalDataBase.java
+++ HospitalDataBase.java
@@ -5,19 +5,33 @@ public class HospitalDataBase {
 
 	private ArrayList<Hospital> allHospitals;
 	private ArrayList<Hospital> allValidHospitals;
+	// current location of the user
+	private Double currentX;
+	private Double currentY;
 
+	public HospitalDataBase(Double x, Double y) {
 
-	public HospitalDataBase(ArrayList<Hospital> allHospitals, String[] currentLocation) {
+		this.allHospitals = new ArrayList<Hospital>();
+		this.allValidHospitals = new ArrayList<Hospital>();
+		this.currentX = x;
+		this.currentY = y;
 
-		this.allHospitals = allHospitals;
-		allValidHospitals = new ArrayList<Hospital>();
+		// create manually 5 Montreal hospitals
+		allHospitals.add(new Hospital("Montreal General Hospital", true, 45.49752494338774, -73.58853601085194, 0));
+		allHospitals.add(new Hospital("Jewish General Hospital", true, 45.49696267784886, -73.63021013836273, 0));
+		allHospitals.add(new Hospital("St. Mary's Hospital", true, 45.4951352243607, -73.62383807250717, 0));
+		allHospitals.add(new Hospital("CHUM", true, 45.51309376029247, -73.5576694104104, 0));
+		allHospitals.add(new Hospital("Catherine Booth Hospital", true, 45.465080168596934, -73.6361520918609, 0));
 
+	}
+
+	
+	public void sortHospitals() {
 		for (Hospital h : allHospitals) {
 			if (h.isHasSpots()) {
 				allValidHospitals.add(h);
 			}
 		}
-
 		// sort the valid hospitals according to their times
 		// insertion sort
 		for (int i = 0; i < allValidHospitals.size(); i++) {
@@ -25,51 +39,43 @@ public class HospitalDataBase {
 			Hospital tmpHospital = allValidHospitals.get(i);
 			int k = i;
 			while (k > 0 && tmpHospital.getMinutesToArrive() < allValidHospitals.get(k - 1).getMinutesToArrive()) {
-				allValidHospitals.set(k, allValidHospitals.get(k-1));
+				allValidHospitals.set(k, allValidHospitals.get(k - 1));
 				k--;
 			}
 			allValidHospitals.set(k, tmpHospital);
 		}
 	}
 
-	public ArrayList<Hospital> getAllHospitals() {
-		return allHospitals;
-	}
+	// return the array list that contains the names of hospitals in order of how
+	// long it would take to get to the hospital
+	public ArrayList<String> getHospitalsNames() {
+		ArrayList<String> arrayString = new ArrayList<String>();
 
-	public void setAllHospitals(ArrayList<Hospital> allHospitals) {
-		this.allHospitals = allHospitals;
+		for (Hospital h : allValidHospitals) {
+			arrayString.add(h.getName());
+		}
+		return arrayString;
 	}
-	
-	public ArrayList<Hospital> getSortedHospitals(){
-		return allValidHospitals;
-	}
-	
-	
-	
 
 	private class Hospital {
 		private String name;
 		private boolean hasSpots;
-		private String[] numSpots; // String of length 2, numAvailableSpots[0] = number of beds currently in use,
-									// numAvailableSpots[1] = total number of beds in a hospital
-		private String[] location; // TODO update so it uses class Location or something that google understands
-		private int minutesToArrive; // how far away is the hospital in minutes
+		private Double x;
+		private Double y;
+		private int time; // how far away is the hospital in minutes
 
-		public Hospital(String name, boolean hasSpots, String[] numSpots, String[] location, int min) {
+		public Hospital(String name, boolean hasSpots, Double x, Double y, int time) {
 			this.name = name;
 			this.hasSpots = hasSpots;
-			this.numSpots = numSpots;
-			this.location = location;
-			this.minutesToArrive = min;
+			this.x = x;
+			this.y =y;
+
+			this.time = time;
 		}
 
 		// setters and getters start
 		public String getName() {
 			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
 		}
 
 		public boolean isHasSpots() {
@@ -80,28 +86,13 @@ public class HospitalDataBase {
 			this.hasSpots = hasSpots;
 		}
 
-		public String[] getNumSpots() {
-			return numSpots;
-		}
-
-		public void setNumSpots(String[] numSpots) {
-			this.numSpots = numSpots;
-		}
-
-		public String[] getLocation() {
-			return location;
-		}
-
-		public void setLocation(String[] location) {
-			this.location = location;
-		}
 
 		public int getMinutesToArrive() {
-			return minutesToArrive;
+			return time;
 		}
 
 		public void setMinutesToArrive(int minutesToArrive) {
-			this.minutesToArrive = minutesToArrive;
+			this.time = minutesToArrive;
 		}
 
 		// setters and getters end
